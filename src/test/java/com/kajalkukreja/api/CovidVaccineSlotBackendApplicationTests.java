869ced6ea6d232f2a2c1ffcbf6b9f33b3abac725
Kajal Kukreja <kajalkukreja694@gmail.com>
2021-05-13 00:33:03 +0530

    Creating webservice for covid-vaccine-slot application

---
+++ src/test/java/com/kajalkukreja/api/CovidVaccineSlotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kajalkukreja.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidVaccineSlotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
